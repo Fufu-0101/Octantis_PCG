8e5123b43c5533fa6098c83d2a7ddd8af367b059
Fufu-0101 <2512247734@qq.com>
2024-07-20 19:13:42 +0800

    AddToolUI

--- Source/Octantis/Public/Oct/UI/OctToolBox.cpp
+++ Source/Octantis/Public/Oct/UI/OctToolBox.cpp
@@ -17,10 +17,79 @@ void SOctToolBox::Construct(const FArguments& InArgs)
 		.HAlign(HAlign_Fill)
 		.VAlign(VAlign_Fill)
 		[
-			SNew(SButton)
-			.HAlign(HAlign_Fill)
-			.VAlign(VAlign_Fill)
-			.Text(FText::FromString(ToolName))
+			SNew(SBorder)
+		.BorderBackgroundColor(FLinearColor::Gray)
+		.Padding(10)
+		[
+			SNew(SHorizontalBox)
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			[
+				SNew(SBox)
+				.WidthOverride(100)
+				.HeightOverride(100)
+				[
+					SNew(STextBlock)
+					.Text(FText::FromString("ICON"))
+					.ColorAndOpacity(FLinearColor::Red)
+				]
+			]
+			+ SHorizontalBox::Slot()
+			.FillWidth(1)
+			[
+				SNew(SVerticalBox)
+				+ SVerticalBox::Slot()
+				.AutoHeight()
+				[
+					SNew(SBox)
+					.HAlign(HAlign_Fill)
+					.VAlign(VAlign_Fill)
+					.Padding(0,0,0,10)
+					[
+						SNew(STextBlock)
+						.Text(FText::FromString(ToolName))
+						.Font(FCoreStyle::GetDefaultFontStyle("Regular", 15))
+						
+						.ColorAndOpacity(FLinearColor::Gray)
+					]
+					
+				]
+				+ SVerticalBox::Slot()
+				.FillHeight(1)
+				[
+					SNew(SBorder)
+					.BorderBackgroundColor(FLinearColor(FColor(15, 15, 15)))
+					[
+						SNew(STextBlock)
+						.Text(FText::FromString("Tips"))
+						.ColorAndOpacity(FLinearColor::White)
+					]
+				]
+			]
+			+ SHorizontalBox::Slot()
+			.AutoWidth()
+			[
+				SNew(SVerticalBox)
+				+ SVerticalBox::Slot()
+				.AutoHeight()
+				[
+					SNew(STextBlock)
+					.Text(FText::FromString("HELP"))
+					.ColorAndOpacity(FLinearColor::Gray)
+				]
+				+ SVerticalBox::Slot()
+				.AutoHeight()
+				[
+					SNew(SButton)
+					.Text(FText::FromString("--LOAD--"))
+					
+				]
+			]
+		]
+			// SNew(SButton)
+			// .HAlign(HAlign_Fill)
+			// .VAlign(VAlign_Fill)
+			// .Text(FText::FromString(ToolName))
 		]
 	];
 
