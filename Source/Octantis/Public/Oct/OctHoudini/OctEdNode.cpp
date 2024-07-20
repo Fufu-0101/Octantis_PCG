27baf31309faf21669efeca62ce8525c54988aea
Fufu-0101 <2512247734@qq.com>
2024-07-20 14:37:26 +0800

    Add CurveEdNode&MaskEdNode class

---
+++ Source/Octantis/Public/Oct/OctHoudini/OctEdNode.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Oct/OctHoudini/OctEdNode.h"
+
+
+OctEdCurveNode::OctEdCurveNode()
+{
+}
+
+OctEdCurveNode::~OctEdCurveNode()
+{
+}
+
+OctEdMaskNode::OctEdMaskNode()
+{
+}
+
+OctEdMaskNode::~OctEdMaskNode()
+{
+}
---
+++ Source/Octantis/Public/Oct/OctHoudini/OctEdNode.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OctNodeBase.h"
+
+/**
+ * 
+ */
+class OCTANTIS_API OctEdCurveNode :public UOctNodeBase
+{
+public:
+	OctEdCurveNode();
+	~OctEdCurveNode();
+};
+
+class OCTANTIS_API OctEdMaskNode:public UOctNodeBase
+{
+public:
+	OctEdMaskNode();
+	~OctEdMaskNode();
+};
